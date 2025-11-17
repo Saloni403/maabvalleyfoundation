df206b98c643f4c1272d6d4b8dd54cafb16f7d46
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-11-17 17:43:56 +0530

    link changes

--- web/js/script.js
+++ web/js/script.js
@@ -10,3 +10,20 @@ window.addEventListener("scroll", function() {
     header.classList.remove("sticky");
   }
 });
+
+
+let message = "Hello! I want to know more about your services."; // your custom text
+    let encodedMsg = encodeURIComponent(message);
+document.querySelectorAll(".whatsappbtn").forEach(whatsapp=>{
+   whatsapp.style.cursor="pointer";
+  whatsapp.addEventListener('click',()=>{
+ window.open(`https://wa.me/919086000098?text=${encodedMsg}`, '_blank')
+  })
+})
+
+document.querySelectorAll(".callbtn").forEach(call=>{
+  call.style.cursor="pointer";
+  call.addEventListener('click',()=>{
+ window.open('tel:+919086000098', '_blank')
+  })
+})
