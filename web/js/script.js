92adfd98443ba6f85715c4aaa4b98d28deec1b0f
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-11-18 17:53:38 +0530

    cd

--- web/js/script.js
+++ web/js/script.js
@@ -14,7 +14,7 @@ window.addEventListener("scroll", function() {
 
 let message = "Hello! I want to know more about your services."; // your custom text
     let encodedMsg = encodeURIComponent(message);
-document.querySelectorAll(".whatsappbtn").forEach(whatsapp=>{
+document.querySelectorAll(".bi-whatsapp").forEach(whatsapp=>{
    whatsapp.style.cursor="pointer";
   whatsapp.addEventListener('click',()=>{
  window.open(`https://wa.me/919086000098?text=${encodedMsg}`, '_blank')
@@ -25,5 +25,20 @@ document.querySelectorAll(".callbtn").forEach(call=>{
   call.style.cursor="pointer";
   call.addEventListener('click',()=>{
  window.open('tel:+919086000098', '_blank')
+  })
+})
+
+
+document.querySelectorAll(".bi-facebook").forEach(fb=>{
+  fb.style.cursor="pointer";
+  fb.addEventListener('click',()=>{
+ window.open('https://www.facebook.com/share/1BbxDncien/?mibextid=wwXIfr', '_blank')
+  })
+})
+
+document.querySelectorAll(".bi-instagram").forEach(insta=>{
+  insta.style.cursor="pointer";
+  insta.addEventListener('click',()=>{
+ window.open('https://www.instagram.com/maab_valley_foundation_?igsh=eDQ0azNwdDRjbnlh', '_blank')
   })
 })
